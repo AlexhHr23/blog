92f4f67844d90b361bc012f29e2a51d7e52d1d57
Alex Hernandez <alex.hernandez24kb@gmail.com>
2021-07-07 17:02:54 -0500

    Cargando el Artículo Completo en su propia Página

--- views/single.view.php
+++ views/single.view.php
@@ -3,16 +3,12 @@
     <div class="contenedor">
         <div class="post">
            <article>
-            <h2 class="titulo">Titulo del articulo</h2>
-             <p class="fecha">1 de Enero de 2016</p>
+            <h2 class="titulo"><?php echo $post['titulo']; ?>Titulo del articulo</h2>
+             <p class="fecha"><?php echo fecha($post['fecha']); ?></p>
              <div class="thumb">
-               <a href="#">
-                 <img src="<?php echo RUTA; ?>/imagenes/1.jpeg" alt="">
-               </a>
+                 <img src="<?php echo RUTA; ?>/imagenes/<?php echo $post['thumb']; ?>" alt="<?php echo $post['titulo']; ?>">
              </div>
-             <p class="extracto">La llegada del Home Office
-              Los avances tecnológicos nos permiten estar en nuestro trabajo, aunque estemos lejos físicamente. Queremos y necesitamos ser productivos haciendo nuestro trabajo lo mejor posible, y esto se logra en gran parte con estrategias como el Home Office.
-              Este trabajo en casa o Teletrabajo, es la actividad remunerada que realiza una persona para una empresa desde un lugar alejado de sus instalaciones</p>
+             <p class="extracto"><?php echo $post['texto']; ?></p>
            </article>
         </div>
     </div>
