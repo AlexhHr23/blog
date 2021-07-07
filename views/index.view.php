c52a243b5680519829fc04a2b163ac96ee441cb1
Alex Hernandez <alex.hernandez24kb@gmail.com>
2021-07-06 22:51:47 -0500

    Acabando pagina de inicio

--- views/index.view.php
+++ views/index.view.php
@@ -1,37 +1,35 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Oswald&display=swap" rel="stylesheet">
-    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="<?php echo RUTA; ?>css/estilos.css">
-    <title>Blog Alex</title>
-</head>
-<body>
-    <header>
-      <div class="contenedor">
-        <div class="logo izquierda">
-          <p><a href="#">Mi primer blog</a></p>
+<?php require 'header.php'; ?>
+
+    <div class="contenedor">
+        <div class="post">
+           <article>
+             <h2 class="titulo">Titulo del articulo</h2>
+             <p class="fecha">1 de Enero de 2016</p>
+             <div class="thumb">
+               <a href="#">
+                 <img src="<?php echo RUTA; ?>/imagenes/1.jpeg" alt="">
+               </a>
+             </div>
+             <p class="extracto">Hablar de Home Office es hablar de trabajo en casa o Teletrabajo, pues como bien es sabido, estamos viviendo una época de muchos cambios y avances tecnológicos.</p>
+             <a href="#" class="continuar">Continuar Leyendo</a>
+           </article>
         </div>
 
-        <div class="derecha">
-          <form name="busqueda" class="buscar" action="<?php echo RUTA;?>">
-            <input type="text" name="busqueda" placeholder="Buscar">
-            <button type="submit" class="icono fa fa-search"></button>
-          </form>
-
-          <nav class="menu">
-             <ul>
-               <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-               <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-               <li><a href="#">Contacto<i class="icono fa fa-envelope"></i></a></li>
-             </ul>
-          </nav>
+        <div class="post">
+           <article>
+             <h2 class="titulo">Titulo del articulo</h2>
+             <p class="fecha">1 de Enero de 2016</p>
+             <div class="thumb">
+               <a href="#">
+                 <img src="<?php echo RUTA; ?>/imagenes/1.jpeg" alt="">
+               </a>
+             </div>
+             <p class="extracto">Hablar de Home Office es hablar de trabajo en casa o Teletrabajo, pues como bien es sabido, estamos viviendo una época de muchos cambios y avances tecnológicos.</p>
+             <a href="#" class="continuar">Continuar Leyendo</a>
+           </article>
         </div>
-      </div>
-    </header>
-</body>
-</html>
+
+        <?php require 'paginacion.php'; ?>
+    </div>
+    
+ <?php require 'footer.php'; ?>
